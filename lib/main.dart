cc8d0c27c724a72a6bb721a9c9e9aaba9fec5852
devMubar <shileabdulsalam@gmail.com>
2023-05-09 21:17:54 +0100

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         //primarySwatch: Colors.blue,
       ),
-      home: const PageThree(),
+      home: const MyHomePage( title: "Flutter Demo Home Page",),
     );
   }
 }
